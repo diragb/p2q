757f886685642806beeb87ca568e2d52e559e9db
diragb <hey@diragb.dev>
2023-01-12 06:17:38 +0530

    🗃️ added state management

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,52 @@
 export type ID = string | number;
 export type Listener = (from: ID, data: any) => void;
-export declare class p_p {
-    private p_q;
+export type StoreListener = (data: Object) => void;
+export declare enum MESSAGE_TYPE {
+    NORMAL = 0,
+    STORE = 1,
+    QUERY = 2
+}
+export declare const p2qbc: BroadcastChannel;
+export declare class P2P {
+    private p2q;
     private id;
     private callback;
+    private storeCallback;
+    private response;
     constructor(props: {
-        p_q: p_q;
+        p2q: P2Q;
         id: ID;
     });
     private hear;
     listen(callback: Listener): void;
-    tell(to: ID, data: any): void;
-    broadcast(data: any): void;
+    tell(to: ID, data: any, options?: {
+        global?: boolean;
+    }): void;
+    broadcast(data: any, options?: {
+        global?: boolean;
+    }): void;
     unsubscribe(): void;
+    hearStore(data: any): void;
+    listenToStore(callback: StoreListener): void;
+    ask(options?: {
+        global?: boolean;
+    }): void;
+    setupResponseHandler(response: (to: ID, type: MESSAGE_TYPE, p2p: P2P) => any): void;
+    private respond;
 }
-export declare class p_q {
+export declare class P2Q {
     ids: ID[];
-    existers: Map<ID, p_p>;
-    listeners: Map<ID, p_p>;
-    register(id: ID): p_p | undefined;
+    existers: Map<ID, P2P>;
+    listeners: Map<ID, P2P>;
+    store: {};
+    storeListeners: Map<ID, P2P>;
+    discriminant: ((data: Object) => boolean) | null;
+    register(id: ID): P2P | undefined;
+    createStore(data: Object, discriminant?: (data: Object) => boolean): any;
+    getStore(): {};
+    setStore(data: Object, options?: {
+        global?: boolean;
+    }): boolean;
 }
-declare const p2q: p_q;
+declare const p2q: P2Q;
 export default p2q;
